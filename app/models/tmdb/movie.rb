6acd34d7bd4df8256bd13615f5d4ca353e88a438
Kostya Dubinin <dubininkostya@gmail.com>
2015-09-27 01:09:30 +0300

    Remove stuff that is not used

--- app/models/tmdb/movie.rb
+++ app/models/tmdb/movie.rb
@@ -2,22 +2,6 @@ module TMDB
   class Movie < Base
     APPEND_TO_RESPONSE = "credits,videos,similar".freeze
 
-    def self.now_playing
-      get("/movie/now_playing")
-    end
-
-    def self.popular
-      get("/movie/popular")
-    end
-
-    def self.top_rated
-      get("/movie/top_rated")
-    end
-
-    def self.upcoming
-      get("/movie/upcoming")
-    end
-
     def self.find(id)
       get("/movie/#{id}", query: { append_to_response: APPEND_TO_RESPONSE })
     end
