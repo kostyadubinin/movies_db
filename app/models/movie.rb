850a58a31a902083f86ad280e7afb0364e52b05d
Kostya Dubinin <dubininkostya@gmail.com>
2015-09-27 16:20:08 +0300

    Fix Movie validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,8 @@ class Movie < ActiveRecord::Base
   validates :budget, :revenue, :runtime, allow_nil: true,
             numericality: { only_integer: true, greater_than: 0 }
 
-  validates :tmdb_id, :imdb_id, presence: true, uniqueness: true
+  validates :tmdb_id, presence: true, uniqueness: true
+  validates :imdb_id, uniqueness: true
   validates :popularity, :vote_average, numericality: true
   validates :vote_count, numericality: { only_integer: true }
 end
