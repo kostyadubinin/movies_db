843921f6bb13f813712cceb90a57592e66d24f81
Kostya Dubinin <dubininkostya@gmail.com>
2015-09-27 19:35:45 +0300

    Remove redundant job

--- app/jobs/sync_movie_job.rb
+++
@@ -1,9 +0,0 @@
-class SyncMovieJob < ActiveJob::Base
-  queue_as :default
-
-  def perform(tmdb_id:)
-    attrs = TMDB::Movie.find(tmdb_id)
-    movie = Movie.find_or_initialize_by(tmdb_id: attrs[:tmdb_id])
-    movie.update(attrs)
-  end
-end
