09d8335760a20efe2ed62164ddf85684fbac5d40
Kostya Dubinin <dubininkostya@gmail.com>
2015-09-27 17:44:21 +0300

    Add support for top rated api call

--- app/models/tmdb/movie.rb
+++ app/models/tmdb/movie.rb
@@ -8,6 +8,11 @@ module TMDB
       new(id).find
     end
 
+    def self.top_rated
+      movies_attrs = get("/movie/top_rated")["results"]
+      movies_attrs.map { |movie_attrs| new(movie_attrs["id"]).find }
+    end
+
     def find
       path = "/movie/#{id}"
       response = self.class.get(path, query: { append_to_response: DEFAULTS })
