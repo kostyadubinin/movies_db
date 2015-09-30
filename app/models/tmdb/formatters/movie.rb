95c5f1ee37b8f7e356300fcb02c62d432daa4c79
Kostya Dubinin <dubininkostya@gmail.com>
2015-09-30 23:47:12 +0300

    Improve movie formatter

--- app/models/tmdb/formatters/movie.rb
+++ app/models/tmdb/formatters/movie.rb
@@ -1,26 +1,63 @@
 class TMDB::Formatters::Movie
-  static_facade :prepare, :movie_attrs
+  attr_private :attributes
+
+  def self.prepare(*params)
+    new(*params).prepare
+  end
+
+  def initialize(attributes)
+    @attributes = attributes.deep_symbolize_keys
+  end
 
   def prepare
-    prepared_attrs = attrs.slice(:budget, :imdb_id, :overview, :popularity,
-                                 :poster_path, :release_date, :revenue,
-                                 :runtime, :tagline, :title, :vote_average,
-                                 :vote_count)
+    attrs = attributes.slice(
+      :overview,
+      :popularity,
+      :poster_path,
+      :release_date,
+      :tagline,
+      :title,
+      :vote_average,
+      :vote_count
+    )
 
-    prepared_attrs[:budget]  = zero_to_nil(prepared_attrs[:budget])
-    prepared_attrs[:revenue] = zero_to_nil(prepared_attrs[:revenue])
-    prepared_attrs[:runtime] = zero_to_nil(prepared_attrs[:runtime])
-    prepared_attrs.merge(tmdb_id: attrs[:id])
+    attrs.merge(
+      budget: budget,
+      revenue: revenue,
+      runtime: runtime,
+      imdb_id: imdb_id,
+      tmdb_id: tmdb_id
+    )
+  end
+
+  def tmdb_id
+    attributes[:id]
+  end
+
+  def budget
+    zero_to_nil(attributes[:budget])
+  end
+
+  def revenue
+    zero_to_nil(attributes[:revenue])
+  end
+
+  def runtime
+    zero_to_nil(attributes[:runtime])
+  end
+
+  def imdb_id
+    blank_to_nil(attributes[:imdb_id])
   end
 
   private
 
-  def attrs
-    @attrs ||= movie_attrs.deep_symbolize_keys
-  end
-
   def zero_to_nil(value)
     return if value.nil?
     value.zero? ? nil : value
+  end
+
+  def blank_to_nil(value)
+    value.blank? ? nil : value
   end
 end
