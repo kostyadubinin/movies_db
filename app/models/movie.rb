adf2c15ec51dddca269fe490e3a66a4b57884f9f
Kostya Dubinin <dubininkostya@gmail.com>
2015-09-30 23:53:46 +0300

    Allow empty imdb_id

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,7 @@ class Movie < ActiveRecord::Base
             numericality: { only_integer: true, greater_than: 0 }
 
   validates :tmdb_id, presence: true, uniqueness: true
-  validates :imdb_id, uniqueness: true
+  validates :imdb_id, uniqueness: true, allow_nil: true
   validates :popularity, :vote_average, numericality: true
   validates :vote_count, numericality: { only_integer: true }
 end
